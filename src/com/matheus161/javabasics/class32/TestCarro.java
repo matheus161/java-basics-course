eab6bc0c84d5cf118d208495afd804eea1c7d523
Matheus <matheeus161@gmail.com>
2025-07-02 09:45:33 -0300

    feat: add clas 32 with getters and setters examples

---
+++ src/com/matheus161/javabasics/class32/TestCarro.java
@@ -0,0 +1,9 @@
+package com.matheus161.javabasics.class32;
+
+public class TestCarro {
+    public static void main(String[] args) {
+        Car van = new Car();
+        van.setMarca("Fiat");
+        System.out.println("Marca: " + van.getMarca());
+    }
+}
