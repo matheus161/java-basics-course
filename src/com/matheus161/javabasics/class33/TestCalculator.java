ddc60f3560600e4a858a4aac0d68854fe3ac796e
Matheus <matheeus161@gmail.com>
2025-07-03 08:57:36 -0300

    class 33: methods overload

---
+++ src/com/matheus161/javabasics/class33/TestCalculator.java
@@ -0,0 +1,10 @@
+package com.matheus161.javabasics.class33;
+
+public class TestCalculator {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        calc.soma(1, 2);
+        calc.soma(1.0, 2.0);
+    }
+}
